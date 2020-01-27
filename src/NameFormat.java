93f5b0f671c65ee7a61ae2f70f95bc0d14e3b371
Meg Prescott <mprescott@ccsnh.edu>
2020-01-27 10:36:56 -0500

    Another variation on NameFormat

--- src/NameFormat.java
+++ src/NameFormat.java
@@ -3,49 +3,28 @@ import java.util.Scanner;
 public class NameFormat {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        // At this point, you will be reading only one line of input
-        // The next three lines of code require 3 separate lines of input,
-        // otherwise an exception will be thrown (this is because you are
-        // using the nextLine() method specifically
-
-        /*
-        String firstName = input.nextLine();
-        String middleName = input.nextLine();
-        String lastName = input.nextLine();
-        */
-
-        // If you need to read individual strings on a single line of input
-        // (i.e. individual strings of characters separated by whitespace)
-        // then one option is to use the next() method instead. This is an
-        // acceptable strategy since you will only ever have two or three names
-        // in your input (though it would be good to also think about people
-        // with only one name (Cher, Bono, etc) ;P
-
-        // Here's how you could handle that:
         String firstName;
-        String middleName = null;
+        String middleName;
         String lastName;
         String temp;
 
         firstName = input.next();
         temp = input.next();
+        lastName = input.nextLine(); //this finalizes the line of input and will be empty if there is no third name
 
-        if (input.hasNext()) {
-            middleName = temp;
-            lastName = input.next();
-        } else {
+        if (lastName.isBlank()) {
             lastName = temp;
+            middleName = null;
+        } else {
+            middleName = temp;
         }
 
-        // Though not explicitly stated, the tests are expecting a newline at the end
-        // of the output.
         if (middleName == null){
             System.out.println(lastName + ", " + firstName);
-        }
-        else{
+        } else {
             System.out.println(lastName + ", " + firstName + " " + middleName.charAt(0) + ".");
         }
+
     }
 }
 
