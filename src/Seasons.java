ead0bfcc4dbe603a11bae87d756364d538cabf6a
Meg Prescott <mprescott@ccsnh.edu>
2020-01-27 13:28:51 -0500

    Starting a new variation on Seasons

--- src/Seasons.java
+++ src/Seasons.java
@@ -6,110 +6,22 @@ public class Seasons {
         String inputMonth;
         int inputDay;
 
-        inputMonth = scnr.nextLine();
+        inputMonth = scnr.next();
         inputDay = scnr.nextInt();
 
 
-        if (inputMonth.equals("March")){
-            if (inputDay < 20 && inputDay > 0){
-                System.out.print("Winter");
-            }
-            else if (inputDay >= 20 && inputDay <= 31){
-                System.out.print("Spring");
-            }
-            else{
-                System.out.print("Invalid");
-            }
 
-        }
-        else if (inputMonth.equals("June")){
-            if (inputDay < 20 && inputDay > 0){
-                System.out.print("Spring");
-            }
-            else if (inputDay >= 20 && inputDay <= 30){
-                System.out.print("Summer");
-            }
-            else{
-                System.out.print("Invalid");
-            }
+    }
 
-        }
-        else if (inputMonth.equals("September")){
-            if (inputDay < 22 && inputDay > 0){
-                System.out.print("Summer");
-            }
-            else if (inputDay >= 22 && inputDay <= 30){
-                System.out.print("Autumn");
-            }
-            else{
-                System.out.print("Invalid");
-            }
+    private static boolean isValid(String month, int day) {
+        String[] months = {
+                "January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+                "November", "December"
+        };
 
-        }
-        else if (inputMonth.equals("December")){
-            if (inputDay < 20 && inputDay > 0){
-                System.out.print("Autumn");
-            }
-            else if (inputDay >= 20 && inputDay <= 31){
-                System.out.print("Winter");
-            }
-            else{
-                System.out.print("Invalid");
-            }
-
-        }
-        else if (inputMonth.equals("April")){
-                if (inputDay > 0 && inputDay < 31){
-                    System.out.print("Spring");
-                }
-                else{
-                    System.out.print("Invalid");
-                }
-        }
-        else if (inputMonth.equals("May")){
-            if (inputDay > 0 && inputDay < 32){
-                System.out.print("Spring");
-            }
-            else{
-                System.out.print("Invalid");
-            }
-        }
-        else if (inputMonth.equals("July")){
-            if (inputDay > 0 && inputDay < 32){
-                System.out.print("Summer");
-            }
-            else{
-                System.out.print("Invalid");
-            }
-        }
-        else if (inputMonth.equals("August")){
-            if (inputDay > 0 && inputDay < 32){
-                System.out.print("Summer");
-            }
-            else{
-                System.out.print("Invalid");
-            }
-        }
-        else if (inputMonth.equals("October")){
-            if (inputDay > 0 && inputDay < 32){
-                System.out.print("Autumn");
-            }
-            else{
-                System.out.print("Invalid");
-            }
-        }
-        else if (inputMonth.equals("November")){
-            if (inputDay > 0 && inputDay < 31){
-                System.out.print("Autumn");
-            }
-            else{
-                System.out.print("Invalid");
-            }
-        }
-        else{
-            System.out.print("Invalid");
-        }
+        int[] maxDays = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 
+        return false;
     }
 }
