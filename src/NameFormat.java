47b9ff348299c7e3033fb6e7c343a66532e9f12f
Heath Bennett <heathben418@gmail.com>
2020-01-26 22:30:25 -0500

    PeopleWeights completed

--- src/NameFormat.java
+++ src/NameFormat.java
@@ -3,18 +3,30 @@ import java.util.Scanner;
 public class NameFormat {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String firstName = input.nextLine();
-        String middleName = input.nextLine();
-        String lastName = input.nextLine();
+        String firstName;
+        String middleName = null;
+        String lastName;
+        String temp;
 
-        if (middleName.length() == 0){
-            System.out.print(lastName + ", " + firstName);
+        firstName = input.next();
+        temp = input.next();
+
+
+
+        if (input.hasNext()){
+            middleName = temp;
+            lastName = input.next();
         }
         else{
-            System.out.print(lastName + ", " + firstName + " " + middleName.charAt(0) + ".");
+            lastName = temp;
         }
 
-
+        if (middleName == null){
+            System.out.println(lastName + ", " + firstName);
+        }
+        else{
+            System.out.println(lastName + ", " + firstName + " " + middleName.charAt(0) + ".");
+        }
 
     }
 }
