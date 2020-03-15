3bb272b2d627934bc0a7a7b33a3868282dce3b9a
Heath Bennett <heathben418@gmail.com>
2020-03-15 19:08:27 -0400

    Lab 1.7 SimpleStatistics completed

---
+++ src/SimpleStatistics.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SimpleStatistics {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int num1;
+        int num2;
+        int num3;
+        int num4;
+
+
+        num1 = scnr.nextInt();
+        num2 = scnr.nextInt();
+        num3 = scnr.nextInt();
+        num4 = scnr.nextInt();
+
+        double numA = num1;
+        double numB = num2;
+        double numC = num3;
+        double numD = num4;
+
+        System.out.println((num1 * num2 * num3 * num4) + " " + ((num1 + num2 + num3 + num4)/4));
+        System.out.printf("%.3f", (numA * numB * numC * numD));
+        System.out.printf(" %.3f",(numA + numB + numC + numD)/4);
+        System.out.println("");
+
+    }
+}
