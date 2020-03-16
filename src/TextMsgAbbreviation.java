4b8cb85bb9a0813744be35c94e4a8179aba11206
Heath Bennett <heathben418@gmail.com>
2020-03-16 15:45:37 -0400

    Lab 1.20 TextMsgAbbreviation completed

---
+++ src/TextMsgAbbreviation.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class TextMsgAbbreviation {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("Input an abbreviation:");
+        String userInput = scnr.next();
+
+        if (userInput.equals("LOL")){
+            System.out.println("laughing out loud");
+        }
+        else if (userInput.equals("IDK")){
+            System.out.println("I don't know");
+        }
+        else if (userInput.equals("BFF")){
+            System.out.println("best friends forever");
+        }
+        else if (userInput.equals("IMHO")){
+            System.out.println("in my humble opinion");
+        }
+        else if (userInput.equals("TMI")){
+            System.out.println("too much information");
+        }
+        else{
+            System.out.println("Unknown");
+        }
+
+    }
+}
