f44cac443a2fd140e71b532f22a9d6a87b45c3bd
Heath Bennett <heathben418@gmail.com>
2020-03-15 21:07:58 -0400

    Lab 1.10 MadLib completed

---
+++ src/MadLib.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MadLib{
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        String firstName;
+        String genericLocation;
+        int wholeNumber;
+        String pluralNoun;
+
+        firstName = scnr.next();
+        genericLocation = scnr.next();
+        wholeNumber = scnr.nextInt();
+        pluralNoun = scnr.next();
+
+        System.out.println(firstName + " went to " + genericLocation + " to buy " + wholeNumber + " different types of " + pluralNoun + ".");
+    }
+}
